f166a12e749093a331bbb8c0b9402ebf01a21cf0
Cawe Coy <cawecoy@gmail.com>
2017-05-01 00:22:56 -0300

    client js labrary and sample

---
+++ public/client/rd.js
@@ -0,0 +1,120 @@
+var url_base = "https://cawerd.herokuapp.com"
+
+function getDateTime() {
+    var now     = new Date(); 
+    var year    = now.getFullYear();
+    var month   = now.getMonth()+1; 
+    var day     = now.getDate();
+    var hour    = now.getHours();
+    var minute  = now.getMinutes();
+    var second  = now.getSeconds(); 
+    if(month.toString().length == 1) {
+        var month = '0'+month;
+    }
+    if(day.toString().length == 1) {
+        var day = '0'+day;
+    }   
+    if(hour.toString().length == 1) {
+        var hour = '0'+hour;
+    }
+    if(minute.toString().length == 1) {
+        var minute = '0'+minute;
+    }
+    if(second.toString().length == 1) {
+        var second = '0'+second;
+    }   
+    var dateTime = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second;   
+     return dateTime;
+}
+
+function generateUUID () { // Public Domain/MIT
+    var d = new Date().getTime();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+        d += performance.now(); //use high-precision timer if available
+    }
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+$("form").submit(function(e){
+    var user = {
+    	user:{
+    		'name': $(this).find('input[name="name"]').val(),
+    		'email': $(this).find('input[name="email"]').val(),
+    		'uid': $(this).find('input[name="uid"]').val(),
+    		'url': $(this).find('input[name="url"]').val()
+    	}
+    };
+
+    try{
+		$.ajax({
+			type: 'POST',
+			url: url_base+'/users.json',
+			data: user
+		})
+		.done(function(response){
+			console.log(response);
+		})
+		.fail(function(response){
+			console.log(response);
+		});
+    }
+    catch(e){
+		console.log(e)
+	}
+	
+	return true;
+});
+
+$(document).ready(function() {
+	if (typeof(Storage) !== "undefined") {
+		uid = localStorage.uid;
+		if (!uid) {
+			localStorage.uid = uid = generateUUID();
+		}
+	}
+	else {
+		console.log('Web Storage não suportado.')
+	}
+
+	$('<input>').attr({
+		type: 'hidden',
+		name: 'uid',
+		value: uid
+	}).appendTo('form');
+
+	$('<input>').attr({
+		type: 'hidden',
+		name: 'url',
+		value: window.location.href.split('?')[0]
+	}).appendTo('form');
+
+	var page = { 
+		page: {
+			title: document.title, 
+			url: window.location.href.split('?')[0], 
+			uid: uid, 
+			access_at: getDateTime() 
+		}
+	};
+	
+	try{
+		$.ajax({
+			type: 'POST',
+			url: url_base+'/pages.json',
+			data: page
+		})
+		.done(function(response){
+			console.log(response);
+		})
+		.fail(function(response){
+			console.log(response);
+		});
+	}
+	catch(e){
+		console.log(e)
+	}
+});
