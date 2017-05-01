56cff4acdfa1f0b1f14c4a8458069463fe4dafdb
Cawe Coy <cawecoy@gmail.com>
2017-05-01 00:51:35 -0300

    route / to /user (main feature)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :pages
   resources :users
+
   get '/', :to => redirect('/users')
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
